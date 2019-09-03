a47ac76bd8cd8c89805f148814c1ccb0e480fd25
Huu Bao Nguyen <baonh@omnicasa.com>
2019-09-03 22:55:08 +0700

    commit create Base Response

---
+++ ServerChatOmnicasa/ServerChatOmnicasa/Base/BaseControllerHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ServerChatOmnicasa.Entities;
+
+namespace ServerChatOmnicasa.Base
+{
+    public class BaseControllerHandler : ControllerBase
+    {
+        public BaseResponse<object> Result(object data)
+        {
+            return new BaseResponse<object>
+            {
+                Code = 200,
+                Data = data,
+                Message = "Successful",
+            };
+        }
+
+        public BaseResponse<object> ResultException(Exception ex, int code = 520)
+        {
+            return new BaseResponse<object>
+            {
+                Code = code,
+                Data = null,
+                Message = ex?.Message,
+                Error = ex?.StackTrace
+            };
+        }
+    }
+}
---
+++ ServerChatOmnicasa/ServerChatOmnicasa/Entities/BaseResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace ServerChatOmnicasa.Entities
+{
+    public abstract class BaseResponse
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("error")]
+        public string Error { get; set; }
+    }
+
+    public class BaseResponse<T> : BaseResponse
+    {
+        [JsonProperty("data")]
+        public T Data { get; set; }
+    }
+}
